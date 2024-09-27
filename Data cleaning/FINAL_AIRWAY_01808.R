68ec119c9ec736a2167c5053def73843db543544
B208593 <107573404+B208593@users.noreply.github.com>
2024-09-27 11:47:44 +0200

    Update FINAL_AIRWAY_01808.R

--- Data cleaning/FINAL_AIRWAY_01808.R
+++ Data cleaning/FINAL_AIRWAY_01808.R
@@ -16,9 +16,6 @@ library(ggplot2)
 library(gtsummary)
 library(magrittr)
 
-# CHECKING HOW R STUDIO READS CSV FILE
-AIRWAY_ALL <- read_csv("C:/Users/SST24004/OneDrive - NHS Scotland/Documents/MSc Project/2_Data/1_BINDED_FILES\\AIRWAY_ALL.csv", guess_max=100000)
-
 
 ################ CREATE SUBSET
 ## exclude columns contaiing no data
